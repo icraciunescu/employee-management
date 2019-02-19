821a1d67a6ef3dc03d183031d242c481efbdf264
@sidorb11 <sidorb@yahoo.com>
2019-02-19 09:12:03 +0200

    class departmentDao si department

---
+++ src/main/java/com/sda/dao/DepartmentDao.java
@@ -0,0 +1,25 @@
+package com.sda.dao;
+
+import com.sda.model.Department;
+import com.sda.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.Query;
+import java.util.List;
+
+public class DepartmentDao extends GenericDao<Department> {
+
+    public List<Department> findAll(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Query query = session.createQuery("FROM com.sda.model.Department ");
+
+        List<Department> list = query.getResultList();
+
+        transaction.commit();
+        session.close();
+        return list;
+    }
+}
