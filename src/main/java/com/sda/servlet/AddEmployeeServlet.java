1a84807c5d8d71dfb8b46b249219723baf1d9ace
@sidorb11 <sidorb@yahoo.com>
2019-02-25 10:47:26 +0200

    deleted employee

--- src/main/java/com/sda/servlet/AddEmployeeServlet.java
+++ src/main/java/com/sda/servlet/AddEmployeeServlet.java
@@ -1,7 +1,9 @@
 package com.sda.servlet;
 
+import com.sda.dao.DepartmentDao;
 import com.sda.model.Employee;
 import com.sda.model.User;
+import com.sda.service.DepartmentService;
 import com.sda.service.EmployeeService;
 import com.sda.service.UserService;
 
@@ -12,11 +14,13 @@ import java.io.IOException;
 @WebServlet(value = "/AddEmployee")
 public class AddEmployeeServlet extends HttpServlet {
     private EmployeeService employeeService = new EmployeeService();
+    private DepartmentService departmentService = new DepartmentService();
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Employee employee = new Employee();
         employee.setName(request.getParameter("name"));
+        employee.setDepartment(departmentService.findById(Long.valueOf(request.getParameter("depid"))));
         //employee.setDepartment(request.getParameter("department"));
         employee.setHire_date(request.getParameter("hire-date"));
         employee.setManager(request.getParameter("manager"));
